7a67bec20eabfa7cfff141829d95fca3601068af
milandas2511 <dasmilan.bp@gmail.com>
2026-08-15 00:37:25 +0530

    modify

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://https://ibm-fs-shopkart-u238.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("shopkart_token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
