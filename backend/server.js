e33471a7e1bc307901d9004f8d6b57a11cfccbe6
milandas2511 <dasmilan.bp@gmail.com>
2026-08-15 17:08:56 +0530

    modify

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://ibm-fs-shopkart-u238.onrender.com",
   })
 );
 
